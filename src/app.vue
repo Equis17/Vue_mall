bc1de7ed54039eff35470d85ec086e1aa8c31165
equis <q1158582010@qq.com>
2019-05-25 20:53:55 +0800

    使用mui和mint-ui开发首页

--- src/app.vue
+++ src/app.vue
@@ -3,11 +3,11 @@
         <mt-header fixed title="JD.COM">
         </mt-header>
         <transition mode="out-in">
-            <router-view class=""></router-view>
+            <router-view ></router-view>
         </transition>
         <div>
             <nav class="mui-bar mui-bar-tab">
-                <router-link class="mui-tab-item mui-active" to="/">
+                <router-link class="mui-tab-item" to="/">
                     <span class="mui-icon mui-icon-home"></span>
                     <span class="mui-tab-label">首页</span>
                 </router-link>
@@ -31,7 +31,11 @@
 <script>
     export default {
         name: "app",
-        components:{
+        data: function () {
+            return {
+            }
+        },
+        methods: {
         }
     }
 </script>
@@ -41,26 +45,42 @@
         padding-top: 40px;
     }
 
-    .mint-header, .mui-bar-tab {
+    .mint-header {
         background-color: firebrick;
-    }
-    .mui-bar-tab>.mui-tab-item{
-        color: #fff;
-        &.mui-active{
-            color: firebrick;
-            background-color: #fff;
+
+        .mint-header-button {
+            a {
+                color: #fff;
+                background-color: firebrick;
+            }
         }
     }
 
+    .mui-bar-tab {
+        background-color: firebrick;
 
+        .mui-tab-item {
+            color: #fff;
+            background-color: firebrick;
+        }
+
+        .router-link-exact-active {
+            color: firebrick !important;
+            background-color: #fff !important;
+        }
+    }
 
     .v-enter {
         transform: translateX(100%);
     }
-    .v-leave-to{
+
+    .v-leave-to {
         transform: translateX(-100%);
     }
+
     .v-enter-active, .v-leave-active {
-        transition: all .5s ease;
+        transition: all .1s ease;
     }
+
+
 </style>
--- src/router.js
+++ src/router.js
@@ -1,16 +1,19 @@
 import VueRouter from 'vue-router'
-import homeComponent from "./pages/homeComponent.vue";
-import searchComponent from "./pages/searchComponent.vue";
-import cartComponent from "./pages/cartComponent.vue";
-import infoComponent from "./pages/infoComponent.vue";
+import homeComponent from "./pages/home/homeComponent.vue";
+import searchComponent from "./pages/search/searchComponent.vue";
+import cartComponent from "./pages/cart/cartComponent.vue";
+import infoComponent from "./pages/info/infoComponent.vue";
+import newsList from "./pages/home/newsList.vue"
 
 const router = new VueRouter({
     routes: [
-        {path: '/', component: homeComponent},
-        {path:'/search',component:searchComponent},
-        {path:'/cart',component:cartComponent},
-        {path:'/info',component:infoComponent}
-        ]
+        {path: '/', redirect: '/home'},
+        {path: '/home', component: homeComponent},
+        {path: '/search', component: searchComponent},
+        {path: '/cart', component: cartComponent},
+        {path: '/info', component: infoComponent},
+        {path: '/home/newslist', component: newsList}
+    ]
 });
 
 export default router
